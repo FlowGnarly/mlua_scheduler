08d93746b1ddfa0ebb2e6b6a6985c39b7d806b75
AshleyFlow <highflowey@gmail.com>
2024-11-15 12:24:30 +0330

    remove luau task library implementation

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,23 +1,10 @@
 use clap::Parser;
-use mlua_scheduler::{scheduler::Scheduler, traits::LuaSchedulerMethods};
-use smol::fs;
+use mlua_scheduler::scheduler::Scheduler;
 use std::{env::consts::OS, path::PathBuf};
 
 #[derive(Debug, Parser)]
 struct Cli {
     path: PathBuf,
-}
-
-async fn spawn_script(lua: mlua::Lua, path: PathBuf) -> mlua::Result<mlua::Thread> {
-    let chunk = lua
-        .load(fs::read_to_string(&path).await?)
-        .set_name(fs::canonicalize(&path).await?.to_string_lossy());
-
-    lua.spawn_thread(
-        lua.create_thread(chunk.into_function()?)?,
-        mlua_scheduler::SpawnProt::Spawn,
-        (),
-    )
 }
 
 fn main() {
@@ -29,26 +16,10 @@ fn main() {
         .set("_OS", OS.to_lowercase())
         .expect("Failed to set _OS global");
 
-    mlua_task_std::inject_globals(&lua).unwrap();
-
-    let thread =
-        smol::block_on(spawn_script(lua.clone(), cli.path)).expect("Failed to spawn script");
-
     let scheduler_task = smol::spawn(async move {
         scheduler.run().await.expect("Scheduler failed");
     })
     .fallible();
 
-    match smol::block_on(lua.await_thread(thread)) {
-        Ok(_) => {
-            smol::block_on(scheduler_task);
-
-            std::process::exit(0);
-        }
-        Err(err) => {
-            eprintln!("{err}");
-
-            std::process::exit(1);
-        }
-    }
+    smol::block_on(scheduler_task);
 }
